540691102f986389e65fb922b8c90f8aa2a8b609
Guma96 <mateusz.kutarba96@gmail.com>
2023-07-23 12:53:05 +0200

    new

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { selectError, selectIsLoading } from 'redux/selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { Loader } from './Loader/Loader';
+
+export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div className="container">
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {isLoading && !error && <Loader />}
+      <ContactList />
+    </div>
+  );
+};
